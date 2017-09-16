4985a54cb78a883c0ee745373c855e8298ea71da
Live session user <vanitha96r@gmail.com>
2017-09-16 13:27:00 +0000

    completed login

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,27 +1,47 @@
 package controllers;
 
 import actors.MessageActor;
-import data.Message;
-import play.mvc.*;
+import data.LoginForm;
+import play.Configuration;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.LegacyWebSocket;
+import play.mvc.Result;
+import play.mvc.Results;
+import play.mvc.WebSocket;
+import views.html.index;
+import views.html.login;
 
-import views.html.*;
-
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
+import javax.inject.Inject;
+import java.util.Objects;
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(views.html.index.render());
-    }
-    public LegacyWebSocket<String> chatSocket(){
-        return WebSocket.withActor(MessageActor::props);
-    }
+   @Inject
+   Configuration configuration;
+
+   @Inject
+   FormFactory formFactory;
+
+   public Result index() {
+       return ok(views.html.index.render());
+   }
+
+   public Result login() {
+       return ok(views.html.login.render(""));
+   }
+
+   public Result authenticate(){
+       Form<LoginForm> loginForm = formFactory.form(LoginForm.class).bindFromRequest();
+       if(Objects.equals(configuration.getString("app.user.username"), loginForm.get().getUsername()) &&
+               Objects.equals(configuration.getString("app.user.password"), loginForm.get().getPassword())){
+           return ok();
+       }
+       return Results.unauthorized();
+   }
+
+   public LegacyWebSocket<String> chatSocket() {
+       return WebSocket.withActor(MessageActor::props);
+   }
+
 }
